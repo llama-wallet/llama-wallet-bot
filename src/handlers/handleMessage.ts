bcb2cb615d1a045f9854f28f6fed044872bc0275
0xeabz <eabz@kindynos.mx>
2025-09-28 00:36:02 -0600

    wip

--- src/handlers/handleMessage.ts
+++ src/handlers/handleMessage.ts
@@ -1,13 +1,23 @@
 import { error, success } from "@/constants"
 import { Update } from "@/types"
 
+const START_COMMAND = "/start"
+const CREATE_COMMAND = "/create"
+
 export async function handleMessage(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
   if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") !== env.SECRET_BOT_TOKEN) {
     return new Response(error(403))
   }
 
   const message: Update = await request.json()
-  console.log(message)
 
-  return new Response(success())
+  const text = message.message?.text
+  if (!text) new Response(success())
+
+  switch (text) {
+    case START_COMMAND:
+    case CREATE_COMMAND:
+    default:
+      return new Response(success())
+  }
 }
